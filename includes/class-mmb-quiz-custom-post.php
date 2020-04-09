d6feedc72087e1ccc228bfa0f0cf447d2a44baff
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-09 14:21:24 +0300

    Language adaptation

--- includes/class-mmb-quiz-custom-post.php
+++ includes/class-mmb-quiz-custom-post.php
@@ -102,7 +102,6 @@ class Mmb_Quiz_Custom_Post {
 
 
 	public function replace_default_menu( $args ) {
-
 		if ( is_singular( 'onlinetest' ) || is_tax( 'tematika' ) ) {
 			$args['menu'] = 'quiz menu';
 		}
